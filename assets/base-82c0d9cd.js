5afbfca251482b65740af5955ec299f119cde040
chengchunlin <1875620109@qq.com>
2024-07-27 16:50:34 +0800

    deploy

---
+++ assets/base-82c0d9cd.js
@@ -0,0 +1,8 @@
+import{d as e,o as t,c as p,g as c,h as r,w as u,a as i,i as l,j as _}from"./index-0b8d68bc.js";const d={class:"markdown-body"},k=c(`<h3 id=".vscode%2Fsetting.json" tabindex="-1">.vscode/setting.json</h3><pre class="language-json"><code class="language-json"><span class="token punctuation">{</span>
+  <span class="token property">&quot;liveServer.settings.port&quot;</span><span class="token operator">:</span> <span class="token number">5501</span><span class="token punctuation">,</span>
+  <span class="token property">&quot;editor.codeActionsOnSave&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token property">&quot;source.organizeImports&quot;</span><span class="token operator">:</span> <span class="token string">&quot;explicit&quot;</span><span class="token punctuation">,</span>
+    <span class="token property">&quot;source.fixAll.eslint&quot;</span><span class="token operator">:</span> <span class="token string">&quot;explicit&quot;</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre>`,2),m=[k],g=e({__name:"常用配置",setup(a,{expose:n}){return n({frontmatter:{},excerpt:void 0}),(o,s)=>(t(),p("div",d,m))}}),x={__name:"base",setup(a){return(n,o)=>{const s=_;return t(),r(s,null,{default:u(()=>[i(l(g),{id:"markdown"})]),_:1})}}};export{x as default};
